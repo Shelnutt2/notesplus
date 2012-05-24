0edf571a1ed72e7e97987b89fd967baf6fe66278
Seth Shelnutt <shelnutt2@gmail.com>
2012-05-24 13:07:53 -0400

    Initial repository

---
+++ notesplus/src/shell/edu/notesplus/HomeMenu.java
@@ -0,0 +1,13 @@
+package shell.edu.notesplus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeMenu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
